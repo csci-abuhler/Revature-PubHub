37a2fe0984178f932f6d2ace819642d4f9baa30f
csci-abuhler <equin@uga.edu>
2019-09-21 01:54:15 -0400

    Update TagDAOImpl.java
    - Finished getBooksByTag method
    - Added some comments clarifying what is occuring at some steps
    - Restructured the file to be more organized

--- PubHub Project Files/PubHub-StarterKit/PubHub-StarterKit/src/examples/pubhub/dao/TagDAOImpl.java
+++ PubHub Project Files/PubHub-StarterKit/PubHub-StarterKit/src/examples/pubhub/dao/TagDAOImpl.java
@@ -20,21 +20,14 @@ public class TagDAOImpl implements TagDAO {
 	Connection connection = null;
 	PreparedStatement stmt = null;
 
-	public static void main(String[] args) {
-		TagDAOImpl tag = new TagDAOImpl();
-		//List<String> tags = new ArrayList<>();
-		//tags = tag.getTagsByISBN("1111111111111");
-		// 1111111111111
-		//System.out.println(tags);
-		tag.removeTag("test", "1");
-	} // main
-
+	// Adds a tag to a book
 	@Override
 	public void addTag(String tagName, String isbn) {
 		try {
 			connection = DAOUtilities.getConnection();
 
-			String sql = "insert into book_tags (\"isbn_13\", \"tag_name\") values (\'" + isbn + "\',\'" + tagName + "\')";
+			String sql = "insert into book_tags (\"isbn_13\", \"tag_name\") values (\'" + isbn + "\',\'" + tagName
+					+ "\')";
 
 			stmt = connection.prepareStatement(sql);
 			stmt.executeQuery();
@@ -45,13 +38,17 @@ public class TagDAOImpl implements TagDAO {
 		} // try catch
 	} // addTag
 
+	// Removes a tag from a book
 	@Override
 	public void removeTag(String tagName, String isbn) {
 		try {
+			// Establishes the connection
 			connection = DAOUtilities.getConnection();
-			
-			String sql = "delete from book_tags where book_tags.isbn_13=\'" + isbn + "\' and book_tags.tag_name=\'" + tagName + "\'";
-			
+
+			// SQL statement is made into a string, prepared, and executed
+			String sql = "delete from book_tags where book_tags.isbn_13=\'" + isbn + "\' and book_tags.tag_name=\'"
+					+ tagName + "\'";
+
 			stmt = connection.prepareStatement(sql);
 			stmt.executeQuery();
 		} catch (Exception e) {
@@ -61,10 +58,11 @@ public class TagDAOImpl implements TagDAO {
 		} // try catch
 	} // remove tag
 
+	// Returns all of the tags by a specified ISBN
 	@Override
 	public List<String> getTagsByISBN(String isbn) {
 		List<String> tags = new ArrayList<>();
-		
+
 		try {
 			connection = DAOUtilities.getConnection();
 
@@ -73,27 +71,72 @@ public class TagDAOImpl implements TagDAO {
 			stmt = connection.prepareStatement(sql);
 
 			ResultSet rs = stmt.executeQuery();
-			
+
 			while (rs.next()) {
 				tags.add(rs.getString("tag_name"));
 			} // while
-			
+
 			rs.close();
 		} catch (Exception e) {
 			e.printStackTrace();
 		} finally {
 			closeResources();
 		} // try catch
-		
+
 		return tags;
 	} // getTagsByISBN
 
+	// Returns the books by the specified tag
 	@Override
 	public List<Book> getBooksByTag(String tagName) {
-		// TODO Auto-generated method stub
-		return null;
+		List<String> isbnList = new ArrayList<>();
+		List<Book> books = new ArrayList<>();
+
+		try {
+			connection = DAOUtilities.getConnection();
+
+			// First, we have to find all of the book ISBNs associated with a tag
+			String sql = "select * from book_tags where tag_name = \'" + tagName + "\'";
+			stmt = connection.prepareStatement(sql);
+
+			ResultSet rs = stmt.executeQuery();
+
+			while (rs.next()) {
+				isbnList.add(rs.getString("isbn_13"));
+			} // while
+
+			rs.close();
+			
+			// Now we can narrow down our search since we have the ISBNs
+			String sql2 = "select * from books";
+			stmt = connection.prepareStatement(sql2);
+
+			rs = stmt.executeQuery();
+
+			int i = 0;
+			while (rs.next()) {
+				Book book = new Book();
+
+				book.setIsbn13(isbnList.get(i));
+				book.setAuthor(rs.getString("author"));
+				book.setTitle(rs.getString("title"));
+				book.setPublishDate(rs.getDate("publish_date").toLocalDate());
+				book.setPrice(rs.getDouble("price"));
+				book.setContent(rs.getBytes("content"));
+
+				books.add(book);
+				i++;
+			} // while
+			rs.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			closeResources();
+		} // try catch
+
+		return books;
 	} // getBooksByTag
-	
+
 	private void closeResources() {
 		try {
 			if (stmt != null)
@@ -102,7 +145,7 @@ public class TagDAOImpl implements TagDAO {
 			System.out.println("Could not close statement!");
 			e.printStackTrace();
 		} // try catch
-		
+
 		try {
 			if (connection != null)
 				connection.close();
